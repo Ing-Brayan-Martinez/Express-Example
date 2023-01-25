6464d4ef3fbcefdf593da3da4306dc8a9ecf7654
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2023-01-25 19:38:46 -0400

    actualizar proyecto

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,19 @@
-import {describe, test} from "@jest/globals";
+import {describe, it, afterAll} from "@jest/globals";
 import {app} from "../src";
 import request from "supertest";
 
 describe('Express', () => {
 
-    test('GET /', async () => {
+    // Set timeout for exit
+    afterAll(async () => {
+        await new Promise<void>(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+    });
+
+    it('GET /',  (done) => {
 
         request(app)
             .get('/')
             .expect(200)
-            .end(function(err, res) {
-                if (err) throw err;
-            });
+            .end(done);
     });
 });
