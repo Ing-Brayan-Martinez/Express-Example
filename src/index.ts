24fc7cc7cb3bb22ffbd45951b2f4963def588487
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2023-01-25 19:47:12 -0400

    actualizar proyecto

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import {getResult} from "./controller/hola";
 import Optional from "optional-js";
+import timeout from 'connect-timeout';
+
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ if (result.isPresent()) {
 export const app: Express = express();
 
 const port = process.env.PORT;
+
+app.use(timeout('8s'))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
